b6f18108f4486747f8b793ed9f83682bd9e807cd
Siddami <dharmmy621@gmail.com>
2024-02-14 20:30:46 +0100

    connected js file

---
+++ script.js
@@ -0,0 +1,21 @@
+//generate random numbr
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+//get all changing elements from DOM
+
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+
+//amount of guesses
+let guessCount = 1;
+
+// to reset game
+let resetButton;
+
+
+
